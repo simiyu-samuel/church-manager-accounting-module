a0d287d4d46a3bf0d64f0bc1dd25e92fdc4cd987
simiyu-samuel <simiyusamuel869@gmail.com>
2025-05-28 20:35:52 +0300

    Created new controller method AccountsController

---
+++ church-accounting-module/modules/Accounting/Controllers/AccountsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JournalTxn;
+use Illuminate\Http\Request;
+use App\Models\PaymentAccount;
+
+class AccountsController extends Controller
+{
+}
